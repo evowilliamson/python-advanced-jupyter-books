d5fdfd9cdf1b451b61f7cbc394fc2c65499f0188
evowilliamson <ivo_willemsen@hotmail.com>
2019-12-04 19:49:54 +0100

    Adding function introspection

---
+++ test.py
@@ -0,0 +1,9 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+
+# TODO Make this happen soon
+def func():
+    __dict__["bla"] = "bla"
+    # TODO antoher one
+    # TODO fsdfsf
+
